d4a28737460332659518dd0daca5de20d68e8b2f
Ron Shub <ron.s@singular.net>
2018-06-23 22:59:35 +0300

    best push ever

--- DiscountBankScraper.py
+++ DiscountBankScraper.py
@@ -9,10 +9,10 @@ class DiscountBankScrape(DataSourceScraper):
     def login(self):
         chrome_path = os.getcwd()+'/chromedriver'
         driver = webdriver.Chrome(executable_path=chrome_path)
-        driver.get('https://www.discountbank.co.il/DB/private')
+        driver.get('https://start.telebank.co.il/apollo/core/templates/lobby/masterPage.html#/LOGIN_PAGE')
         time.sleep(5)
-        driver.get_screenshot_as_file("asaf")
-        driver.find_element_by_class_name('retail-log-in-button').click()
+        driver.get_screenshot_as_file("asaf.png")
+        driver.find_element_by_class_name('sendBtn').click()
         print driver.current_url
         pass
 
