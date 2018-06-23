31572a197e319c707542be1fbb440f50b06b6347
aliveanu <asaf.liveanu@taboola.com>
2018-06-23 23:19:23 +0300

    login finish

--- DiscountBankScraper.py
+++ DiscountBankScraper.py
@@ -11,7 +11,15 @@ class DiscountBankScrape(DataSourceScraper):
         driver = webdriver.Chrome(executable_path=chrome_path)
         driver.get('https://start.telebank.co.il/apollo/core/templates/lobby/masterPage.html#/LOGIN_PAGE')
         time.sleep(5)
-        driver.get_screenshot_as_file("asaf.png")
+        username = driver.find_element_by_id("tzId")
+        password = driver.find_element_by_id("tzPassword")
+        aidnum = driver.find_element_by_id("aidnum")
+
+        username.send_keys(os.environ.get("ID"))
+        password.send_keys(os.environ.get("password"))
+        aidnum.send_keys(os.environ.get("identity_code"))
+
+
         driver.find_element_by_class_name('sendBtn').click()
         print driver.current_url
         pass
