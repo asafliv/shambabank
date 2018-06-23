ae2abdf295000bff01af86108f96840ceb98ad33
aliveanu <asaf.liveanu@taboola.com>
2018-06-23 20:08:32 +0300

    webdriver first commit

--- DiscountBankScraper.py
+++ DiscountBankScraper.py
@@ -1,7 +1,22 @@
+import os
 from DataSourceScraper import DataSourceScraper
-
+from selenium import webdriver
 
 class DiscountBankScrape(DataSourceScraper):
 
     def login(self):
+        chrome_path = os.getcwd()+'/chromedriver'
+        driver = webdriver.Chrome(executable_path=chrome_path)
+        driver.get('https://www.discountbank.co.il/DB/private')
         pass
+
+
+def main():
+    x = DiscountBankScrape()
+    x.login()
+
+
+if __name__ == "__main__":
+    main()
+
+print "Guru99"
