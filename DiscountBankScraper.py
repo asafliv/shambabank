9644e71f20b0389e130ce8c4e2a1854363249af2
aliveanu <asaf.liveanu@taboola.com>
2018-06-23 21:59:15 +0300

    ZIBN

--- DiscountBankScraper.py
+++ DiscountBankScraper.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 from DataSourceScraper import DataSourceScraper
 from selenium import webdriver
 
@@ -8,6 +10,10 @@ class DiscountBankScrape(DataSourceScraper):
         chrome_path = os.getcwd()+'/chromedriver'
         driver = webdriver.Chrome(executable_path=chrome_path)
         driver.get('https://www.discountbank.co.il/DB/private')
+        time.sleep(5)
+        driver.get_screenshot_as_file("asaf")
+        driver.find_element_by_class_name('retail-log-in-button').click()
+        print driver.current_url
         pass
 
 
@@ -18,5 +24,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-print "Guru99"
